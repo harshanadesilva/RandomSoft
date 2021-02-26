7b1a78dd10d192e10a4eb430d926676964ad2489
harshanadesilva <79626939+harshanadesilva@users.noreply.github.com>
2021-02-26 08:33:49 +0530

    Add files via upload

---
+++ Service/PersonService.cs
@@ -0,0 +1,54 @@
+﻿using MyApi.IService;
+using MyApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace MyApi.Service
+{
+    public class PersonService : IGenericService<Person>
+    {
+        string Baseurl = "https://60377f0e54350400177228df.mockapi.io/";
+        List<Person> _person = new List<Person>();
+
+        public PersonService()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                _person.Add(new Person()
+                {
+                    Id = i,
+                    FirstName = "FirstName" + i,
+                    LastName = "LastName" + i,
+                    Email = "Email" + i
+                });
+            }
+        }
+
+        public List<Person> Delete(int id)
+        {
+            _person.RemoveAll(x => x.Id == id);
+            return _person;
+        }
+
+        public List<Person> GetAll()
+        {
+            return _person;
+        }
+
+        public Person GetById(int id)
+        {
+            return _person.Where(x => x.Id == id).SingleOrDefault();
+        }
+
+        public List<Person> Insert(Person item)
+        {
+            _person.Add(item);
+            return _person;
+        }
+    }
+}
---
+++ Service/PostService.cs
@@ -0,0 +1,50 @@
+﻿using MyApi.IService;
+using MyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApi.Service
+{
+    public class PostService : IGenericService<Post>
+    {
+        List<Post> _post = new List<Post>();
+
+        public PostService()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                _post.Add(new Post() {
+                    Id = i,
+                    Title = "Title" + i,
+                    ImageUrl = "ImageUrl" + i,
+                    AuthorName = "AuthorName" + i,
+                    TotalLikes =+ i
+                });
+            }
+        }
+
+        public List<Post> Delete(int id)
+        {
+            _post.RemoveAll(x => x.Id == id);
+            return _post;
+        }
+
+        public List<Post> GetAll()
+        {
+            return _post;
+        }
+
+        public Post GetById(int id)
+        {
+            return _post.Where(x=>x.Id==id).SingleOrDefault();
+        }
+
+        public List<Post> Insert(Post item)
+        {
+            _post.Add(item);
+            return _post;
+        }
+    }
+}
